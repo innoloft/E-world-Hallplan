fc21d846a81e00d9b714ee211c72a3dc327bb8ae
Philipp Bischoff <p.bischoff@innoloft.com>
2024-10-22 11:04:32 +0200

    add base structure for favorite handling

--- public/js/handler.js
+++ public/js/handler.js
@@ -1,1 +1,32 @@
-//TODO: Add structure to initialize favorites and handle updates on favorites
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get("token");
+const favorites = urlParams.get("favorites")?.split(",") || [];
+
+InforoMap.on("app/ready", function (e) {
+  // InforoMap.initFavorites(favorites);
+});
+
+InforoMap.on("feature/marker-click", function (e) {
+  const exhibitor = e.exhibitorId;
+  if (favorites.includes(exhibitor)) {
+    removeFavorites(exhibitor);
+  } else {
+    addFavorites(exhibitor);
+  }
+});
+
+const addFavorites = (exhibitorId) => {
+  if (!token) {
+    return;
+  }
+
+  //todo api call to loftos API to add exhibitor to favorites
+};
+
+const removeFavorites = (exhibitorId) => {
+  if (!token) {
+    return;
+  }
+
+  //todo api call to loftos API to remove exhibitor from favorites
+};
